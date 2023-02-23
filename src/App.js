2ae9356bf6372c0d3c3ea11b507fcd6cb38e1139
zygisk123 <zygimantask13@gmail.com>
2023-02-23 15:06:39 +0200

    add logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import Papa from 'papaparse';
+import { useState } from 'react';
+import { Fragment } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [columnArray, setColumnArray] = useState([]);
+  const [values, setValues] = useState([]);
+
+  const fileHandler = (event) =>
+  {
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function(result) {
+        const columnArray = [];
+        const valuesArray = [];
+
+        result.data.map((data) => {
+          columnArray.push(Object.keys(data));
+          valuesArray.push(Object.values(data));
+        })
+        setData(result.data);
+        setColumnArray(columnArray[0]);
+        setValues(valuesArray);
+      }
+    }
+      );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <input
+          type="file"
+          name='file'
+          accept='.csv'
+          onChange={fileHandler}
+          style={{}}>
+          
+        </input>
+      </div>
+      <div>
+        <table >
+          <thead>
+            <tr>
+              {columnArray.map((col, id) => (<th key={id}>{col}</th>))}
+            </tr>
+          </thead>
+          <tbody>
+            {values.map((values,id) =>(
+              <tr key={id}>
+                {values.map((value, i) => (
+                  <td key={i}>{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Fragment>
   );
 }
 
